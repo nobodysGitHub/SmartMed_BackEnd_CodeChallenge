b34056ca0309adcfd063925d03f416ede254ad7b
Francioo87NABA <91459536+nobodysGitHub@users.noreply.github.com>
2021-10-11 17:35:02 +0200

    MedicationControllers

---
+++ SmartMed_BackEnd_CodeChallenge/Controllers/MedicationsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartMed_BackEnd_CodeChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicationsController : ControllerBase
+    {
+    }
+}
