d00446eabf520da3885745ee5bccde3982a607e1
Francioo87NABA <91459536+nobodysGitHub@users.noreply.github.com>
2021-10-11 17:43:40 +0200

    MockMedicationData
    Inherit from Imedication and added the IMedication Structure

---
+++ SmartMed_BackEnd_CodeChallenge/MedicationData/MockMedicationData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartMed_BackEnd_CodeChallenge.MedicationData
+{
+    public class MockMedicationData
+    {
+    }
+}
